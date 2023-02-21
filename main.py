8e4ee5aacb07c9f34da62e3974713b902b0bff22
Victor Buendia <victorbuendia03@gmail.com>
2023-02-21 19:59:06 -0300

    Melhora da legibilidade do código

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
+import os
+import math
+import json
 import imghdr
 import smtplib 
-import email.message
-from datetime import date, timedelta
-import os
 import requests
-import json
-import math
+import email.message
 import matplotlib.pyplot as plt
+from datetime import date, timedelta
 
-def enviar_email(body, precoHoje, receiver):
+global today
+today = date.today()
+
+def load_json(json_data):
+	return json.loads(json_data)
+
+def send_email(todays_price, receiver):
 	corpo_email =\
 	f"""
 	"""
+	
+	rounded_todays_price = math.ceil(todays_price * 100.0) / 100.0
+	todays_day = ('000'+str(date.today().day))[-2:]
+	todays_month = ('000'+str(date.today().month))[-2:]
+	todays_year = str(date.today().year)
+	today = todays_day + '/' + todays_month + '/' + todays_year
 
 	msg = email.message.EmailMessage()
-	msg['Subject'] = "{EUR} "+f"R${math.ceil(precoHoje * 100.0) / 100.0} | " +('000'+str(date.today().day))[-2:]+'/'+('000'+str(date.today().month))[-2:]+'/'+str(date.today().year)
+	msg['Subject'] = "{EUR} " + f"R${rounded_todays_price} | " + today
 	msg['From'] = os.environ['SENDER']
 	msg['To'] = receiver
-	password = os.environ['GMAILKEY']
+	
 	msg.add_header ('Content-Type', 'text/html') 
 	msg.set_payload(corpo_email)
 
@@ -32,73 +44,123 @@ def enviar_email(body, precoHoje, receiver):
 	s.starttls()
 
 	# Login Credentials for sending the mails
+	password = os.environ['GMAILKEY']
 	s.login(msg['From'], password)
-	s.sendmail(msg['From'], msg['To'], msg.as_string().encode ('utf-8'))
-	print('Emails enviados')
 
-def preco_euro(fromc, toc):
-	url = f"https://api.apilayer.com/exchangerates_data/timeseries?start_date={date.today()+timedelta(days = -30)}&end_date={date.today()}&base={fromc}&symbols={toc}"
+	s.sendmail(msg['From'], msg['To'], msg.as_string().encode('utf-8'))
+
+	print('Email Sent!')
+
+def get_time_series(from_currency, to_currency):
+	endDate = date.today()
+	startDate = endDate +timedelta(days = -30)
+
+	url = f"https://api.apilayer.com/exchangerates_data/timeseries?start_date={startDate}&end_date={endDate}&base={from_currency}&symbols={to_currency}"
 
 	payload = {}
 	headers= {
 	"apikey": os.environ['ERDA']
 	}
 
-	response = requests.request("GET", url, headers=headers, data = payload)
+	response = requests.request("GET", url, headers = headers, data = payload)
 
 	status_code = response.status_code
 	result = response.text
 
-	return result, status_code
+	return {'result': result, 'status_code': status_code}
 
-def cria_grafico(data):
-	info = data
+def average(list):
+	return sum(list)/len(list)
 
+def data_to_dict(data_dict, to_currency):
 	dates = []
-	values = []
-	today = []
-	avg = []
+	prices = []
 
-	for i in info['rates'].keys():
-		dates.append(i)
-		values.append(info['rates'][i]['BRL'])
+	data = load_json(data_dict['result'])
+
+	for date in data['rates'].keys():
+		dates.append(date)
+		prices.append((data['rates'][date])[to_currency])
 		
-	for i in dates:
-		today.append(values[-1])
-		avg.append(sum(values)/len(values))
-		
-	fig, ax = plt.subplots(figsize=(10, 6))
+	return {'dates': dates, 'prices': prices}
 
-	plt.title("Cotação do Euro | "+date.today().strftime('%d/%m/%y'), size=15)
-	ax.set(frame_on=False)
+def create_graph(x_size, y_size):
+	fig, ax = plt.subplots(figsize = (x_size, y_size))
+	return fig, ax
 
-	ax.plot(dates, values, 'r')
-	ax.plot(dates, values, 'r.')
-	ax.plot(dates, today, '--g')
-	ax.plot(dates, avg, ':k')
+def draw_graph(dict_data):
+	dates, prices = dict_data['dates'], dict_data['prices']
+
+	average_price = average(prices)
+	todays_price = prices[-1]
+
+	today_list = []
+	avg_list = []
+
+	for date in dates:
+		today_list.append(todays_price)
+		avg_list.append(average_price)
+
+	_, ax = create_graph(10, 6)
+
+	today_date = today.strftime('%d/%m/%y')
+	title = "Cotação do Euro | " + today_date
+
+	plt.title(title, size = 15)
+	ax.set(frame_on = False)
+
+	# Graph Prices
+	ax.plot(dates, prices, 'r')
+	# Graph Dots Prices
+	ax.plot(dates, prices, 'r.')
+	# Today's Price
+	ax.plot(dates, today_list, '--g')
+	# Average Price
+	ax.plot(dates, avg_list, ':k')
 
 	labels = ax.get_xticklabels()
-	plt.setp(labels, 
-			rotation=45, 
-			horizontalalignment='right')
+
+	plt.setp(labels
+	  		, rotation = 45 
+			, horizontalalignment = 'right')
 	ax.yaxis.set_major_formatter('R${x:1.2f}')
 
-	ax.text(dates[-1], values[-1], '   HOJE: R$'+str(math.ceil(values[-1]*100)/100), size=10)
-	ax.text(dates[-1], avg[-1], '   MÉDIA: R$'+str(math.ceil(avg[-1]*100)/100), size=10)
+	# Label for today's price
+	ax.text(dates[-1]
+	 		, prices[-1]
+			, '   HOJE: R$'+str(math.ceil(prices[-1]*100)/100), size = 10)
+	# Label for average price
+	ax.text(dates[-1]
+			, avg_list[-1]
+			, '   MÉDIA: R$'+str(math.ceil(avg_list[-1]*100)/100), size = 10)
 
 	plt.tight_layout()
-	plt.savefig("eurograph.png", bbox_inches="tight")
-	return avg[-1]
+	plt.savefig("eurograph.png", bbox_inches = "tight")
+
 	# plt.show()
 
+def get_today_price(loaded_data, today, to_currency):
+	return loaded_data['rates'][str(today)][to_currency]
+
 if __name__ == '__main__':
-	precos = json.loads(preco_euro('EUR', 'BRL')[0])
-	
-	precoHoje = precos['rates'][str(date.today())]['BRL']
+	from_currency = 'EUR'
+	to_currency = 'BRL'
 
-	avg = cria_grafico(precos)
+	data = get_time_series(from_currency, to_currency)
+	today = date.today()
 
-	if precoHoje < avg:
-		receivers = json.loads(os.environ['RECEIVERS'])
-		for receiver in receivers:
-			enviar_email(precos, precoHoje, receiver)
+	if data['status_code'] == 200:
+		loaded_data = load_json(data['result'])
+		dict_data = data_to_dict(data, to_currency)
+
+		todays_price = get_today_price(loaded_data, today, to_currency)
+		average_price = average(dict_data['prices'])
+
+		draw_graph(dict_data)
+
+		if todays_price < average_price:
+			receivers = json.loads(os.environ['RECEIVERS'])
+			for receiver in receivers:
+				send_email(todays_price, receiver)
+	else:
+		raise Exception('API connection error.')
